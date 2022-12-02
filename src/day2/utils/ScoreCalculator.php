fb4501214702ae20390dbe4a26b4cc7b815afda7
rmfloris <ralffloris@gmail.com>
2022-12-02 08:00:42 +0000

    resolved puzzle day 2, puzzle 2

--- src/day2/utils/ScoreCalculator.php
+++ src/day2/utils/ScoreCalculator.php
@@ -7,14 +7,34 @@ class ScoreCalculator {
     private $moves = array();
     private $inputData = array();
     private $points = [
-        "X" => 1, // Rock
-        "Y" => 2, // Paper
-        "Z" => 3 // Scissors
+        "A" => 1, // Rock
+        "B" => 2, // Paper
+        "C" => 3  // Scissors
+    ];
+    private $strategicOutcome = [
+        "X" => "lose",
+        "Y" => "draw",
+        "Z" => "win"
+    ];
+    private $myMoveOptions = [
+        "win" => [
+            "A" => "B",
+            "B" => "C",
+            "C" => "A"
+        ],
+        "lose" => [
+            "A" => "C",
+            "B" => "A",
+            "C" => "B"
+        ]
     ];
     private $pointsResult = [
         "win" => 6,
         "draw" => 3,
-        "lose" => 0
+        "lose" => 0,
+        "X" => 0,
+        "Y" => 3,
+        "Z" => 6
     ];
 
     public function __construct($inputfile) {
@@ -43,9 +63,16 @@ class ScoreCalculator {
 
     private function calculateScore($moves) {
         $score = 0;
-        $score += $this->points[$moves["me"]];
-        $score += $this->pointsResult[$this->gameResult($moves)];
+        $myMove = $this->defineMove($moves);
+        $score += $this->points[$myMove];
+        $score += $this->pointsResult[$moves["me"]];
         return $score;
+    }
+
+    private function defineMove($moves) {
+        if($this->strategicOutcome[$moves["me"]] == "draw") { return $moves["opponent"]; }
+
+        return $this->myMoveOptions[$this->strategicOutcome[$moves["me"]]][$moves["opponent"]];        
     }
 
     private function gameResult($moves) {
