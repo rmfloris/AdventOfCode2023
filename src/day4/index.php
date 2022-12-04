3fa43bb26675ac0779cdbb35bdad6dc14ee2866e
rmfloris <ralffloris@gmail.com>
2022-12-04 14:12:35 +0000

    Solved puzzle 2, day 4

--- src/day4/index.php
+++ src/day4/index.php
@@ -3,8 +3,10 @@ require_once '../autoload.php';
 
 use day4\utils\Day4;
 
-$file = "input/input_test.txt";
-// $file = "input/input.txt";
+// $file = "input/input_test.txt";
+$file = "input/input.txt";
 
 $numberOfAssignment = new Day4($file);
 echo "Number of overlapping ranges: ". $numberOfAssignment->findNumberOfFullyOverlappingRange(); 
+echo "<br>";
+echo "Number of partly overlapping ranges: ". $numberOfAssignment->findNumberOfPartylOverlappingRange();
--- src/day4/utils/Day4.php
+++ src/day4/utils/Day4.php
@@ -32,16 +32,37 @@ class Day4 {
             $firstElf = explode("-", $data[0]);
             $secondElf = explode("-", $data[1]);
 
-            if($this->checkOverlap($firstElf[0], $firstElf[1], $secondElf[0], $secondElf[1]) || 
-            $this->checkOverlap($secondElf[0], $secondElf[1], $firstElf[0], $firstElf[1])) {
+            if($this->checkFullOverlap($firstElf[0], $firstElf[1], $secondElf[0], $secondElf[1]) || 
+            $this->checkFullOverlap($secondElf[0], $secondElf[1], $firstElf[0], $firstElf[1])) {
                 $number += 1;
             }
         }
         return $number;
     }
 
-    private function checkOverlap($start1, $end1, $start2, $end2) {
+    public function findNumberOfPartylOverlappingRange() {
+        $number = 0;
+        foreach($this->pairContent as $data) {
+            $firstElf = explode("-", $data[0]);
+            $secondElf = explode("-", $data[1]);
+
+            if($this->checkPartlyOverlap($firstElf[0], $firstElf[1], $secondElf[0], $secondElf[1])) {
+                // echo $firstElf[0] ." - ". $firstElf[1] ." - ". $secondElf[0] ." - ". $secondElf[1] ." - ". $this->checkPartlyOverlap($firstElf[0], $firstElf[1], $secondElf[0], $secondElf[1]) ."<br>";
+                $number += 1;
+            }
+        }
+        return $number;
+    }
+
+    private function checkFullOverlap($start1, $end1, $start2, $end2) {
         if($start1 <= $start2 && $end1 >= $end2) { return true; }
         return false;
+    }
+
+    private function checkpartlyOverlap($firstStart, $firstEnd, $secondStart, $secondEnd) {
+        return in_array($firstStart, range($secondStart, $secondEnd)) || 
+        in_array($firstEnd, range($secondStart, $secondEnd)) || 
+        in_array($secondStart, range($firstStart, $firstEnd)) || 
+        in_array($secondEnd, range($firstStart, $firstEnd));
     }
 }
