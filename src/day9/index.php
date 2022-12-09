c8c12ece0a96d52e4b1f1c70a432f7876d3e00c3
rmfloris <ralffloris@gmail.com>
2022-12-09 20:58:01 +0000

    refactor logic to suit puzzle #2

--- src/day9/index.php
+++ src/day9/index.php
@@ -4,12 +4,18 @@ require_once '../autoload.php';
 $filename = "input/input.txt";
 
 use day9\utils\Day9;
+use day9\utils\Day9Part2;
 
 $positions = new Day9($filename);
 $positions->startMoving();
 
-echo "positions: ". $positions->getNumberOfPositions();
+echo "positions: ". $positions->getNumberOfPositions() ."<br>";
 
-// echo "<pre>";
-// var_dump($positions);
+$part2 = new Day9Part2($filename);
+$part2->startMoving();
+echo "positions: ". $part2->getNumberOfPositions(1) ."<br>";
+
+
+// echo "<br><pre>";
+// var_dump($part2);
 
---
+++ src/day9/test.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "<pre>";
+print_r(array_fill(0, 10, ["x" => 0, "y" => 0]));
