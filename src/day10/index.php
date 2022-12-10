179fb884a87d713021027179045843b390cc9385
rmfloris <ralffloris@gmail.com>
2022-12-10 11:36:39 +0000

    resolved puzzle 1, day 10

--- src/day10/index.php
+++ src/day10/index.php
@@ -1,8 +1,15 @@
 <?php
 require_once '../autoload.php';
-// $filename = "input/input_test.txt";
+$filename = "input/input_test.txt";
+$filename = "input/input_test_large.txt";
 $filename = "input/input.txt";
 
 use day10\utils\Day10;
 
-$part2 = new Day10($filename);
+$computer = new Day10($filename);
+$computer->startProgram();
+
+echo "1.Total signal strenght: " . $computer->getSignalStrength() ." (14780)<br>";
+
+echo "<pre>";
+var_dump($computer);
--- src/day10/utils/Day10.php
+++ src/day10/utils/Day10.php
@@ -5,13 +5,63 @@ use common\LoadInput;
 
 class Day10 {
     private $inputArray = [];
+    private $registerX = 1;
+    private $cycle = 1;
+    private $valueAtCycle = [];
 
     public function __construct($filename) {
         $this->inputArray = $this->parseInput($filename);
+        // $this->valueAtCycle[$this->cycle] = $this->registerX;
     }
 
     private function parseInput($inputFile) {
         return explode("\n", (new LoadInput)->loadFile($inputFile));
     }
 
+    public function startProgram() {
+        foreach($this->inputArray as $lines) {
+            [$program, $value] = explode(" ", $lines) + ["1"=>null];
+
+            // echo "Program: ". $program ."<br>";
+            $this->{$program}($value);
+        }
+    }
+
+    private function noop($value) {
+        // echo "noop<br>";
+        $this->addCycle();
+        return true;
+    }
+
+    private function addX($value) {
+        // echo "addX<br>";
+        $this->addCycle(2);
+        $this->setRegisterXValue($value);
+    }
+
+    private function setRegisterXValue($addValue) {
+        return $this->registerX += $addValue;
+    }
+    private function addCycle($numberOfTicks = 1) {
+        // $this->cycle += $numberOfTicks;
+        for($i=0; $i<$numberOfTicks; $i++) {
+            $this->setValueAtCycle();
+            $this->cycle++;            
+        }
+        return true;
+    }
+
+    private function setValueAtCycle() {
+        $this->valueAtCycle[$this->cycle] = $this->registerX;
+        return true;
+    }
+
+    public function getSignalStrength() {
+        $totalSignalStrength = 0;
+        for($i=20;$i<count($this->valueAtCycle);$i+=40) {
+            // echo $i ." - ". $this->valueAtCycle[$i] ." - ". $i * $this->valueAtCycle[$i] ."<br>";
+            $totalSignalStrength += $i * $this->valueAtCycle[$i];
+        }
+        return $totalSignalStrength;
+    }
 }
