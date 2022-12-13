bb95d9f20365d6674943e5e2ee09e485294433a5
rmfloris <ralffloris@gmail.com>
2022-12-13 21:42:34 +0000

    resolved puzzle 1, day 13

--- src/day13/index.php
+++ src/day13/index.php
@@ -3,21 +3,19 @@ require_once '../autoload.php';
 error_reporting(E_ALL & ~E_WARNING);
 use day13\utils\Day13;
 
-// $filename = "input/input_test.txt";
+$filename = "input/input_test.txt";
+$filename = "input/input_sample.txt";
 $filename = "input/input.txt";
+
 echo "<pre>";
 
 $distress = new Day13($filename);
 $distress->startComparing();
 echo "<p>";
 echo "are these the same?<p>";
-echo "Part 1, sum of pairs: ". $distress->getSumOfIndices() ." (first guess 5841, too high)";
-/**
- * value of 5841 is too high
- * number of pairs is correct
- * all pairs have either a 0 or 1 value
- */
+echo "Part 1, sum of pairs: ". $distress->getSumOfIndices() ." (5506)";
 
-echo "<p>Pairs:" . print_r($distress->getPairs(), true) ."<br>";
-// echo "<pre>";
-// var_dump($path);
+$distress->addPackage(json_decode('[[2]]'));
+$distress->addPackage(json_decode('[[6]]'));
+
+var_dump($distress);
--- src/day13/utils/Day13.php
+++ src/day13/utils/Day13.php
@@ -16,7 +16,6 @@ class Day13 {
         $lines = (new LoadInput)->loadFileToLines($filename);
         foreach($lines as $line) {
             if(strlen($line > 0)) {
-                // echo "lineimport: ". print_r(json_decode($line),true) ."<br>";
                 $this->inputData[] = json_decode($line);
             }
         }
@@ -25,9 +24,7 @@ class Day13 {
     public function startComparing() {
         $pair = 1;
         for($i=0; $i<count($this->inputData);$i++) {
-            // echo "<p>== Pair ". $pair ." ==<br>";
-            $this->pairs[$pair] = $this->compareLines($this->inputData[$i], $this->inputData[$i+1]);
-
+            $this->pairs[$pair] = ($this->compareLines($this->inputData[$i], $this->inputData[$i+1]) < 1 ? 1 : 0);
             $i++;
             $pair++;
         }
@@ -45,78 +42,26 @@ class Day13 {
     }
 
     private function compareLines($line1, $line2) {
-        $status = 1;
-
-        // echo "line 1: ". print_r($line1, true) ."<br>";
-        // echo "line 2: ". print_r($line2, true) ."<br>";
-        // echo "- Compare [". implode(",", $line1) ."] vs [". implode(",", $line2) ."]<br>";
-        for($i=0; $i<count($line1);$i++) {
-            // mixed types
-            //
-            // echo "Line: ". print_r($line1, true) ."<br>";
-            if(getType($line1[$i]) != gettype($line2[$i])) {
-                // echo "type different<br>";
-                $status = $this->convertToArrayAndCompare($line1[$i], $line2[$i]);
-                if($status != -1) return $status;
-            }
-            // compare pairs
-            //
-            $status = $this->comparePair($line1[$i], $line2[$i]);
-            if($status != -1) return $status;
-        }
-        if($this->compareArrayLength($i, $line1, $line2) != -1) { return $this->compareArrayLength($i, $line1, $line2); }
-        return $status;
-    }
-
-    private function convertToArrayAndCompare($line1, $line2) {
-        if(is_null($line2)) { return 0; }
-        (is_array($line1) ? $line2 = [$line2] : $line1 = [$line1]);
-        return $this->compareLines($line1, $line2);
-    }
-
-    private function compareArrayLength($position, $array1, $array2) {
-        return (count(array_slice($array1, $position)) == 0 && count(array_slice($array2, $position)) > 0 ? 1: 0);
-    }
-
-    private function comparePair($pair1, $pair2) {
-        // echo "  - Compare ". $pair1 ." vs ". $pair2 ."<br>";
-        if($pair1 == $pair2) return -1;
-        if($pair1 < $pair2) return 1;
-        return 0;
-    }
-
-
-    /**
-     * 
-     */
-
-/*
-    public function comparePackets() {
-        $pair = 1;
-        for($i=0; $i<count($this->inputData);$i++) {
-
-            $rightOrder = $this->inOrderPair(($this->inputData[$i]), $this->inputData[$i+1]);
-            $this->pairs[$pair] = $rightOrder;
-            $i++;
-            $pair++;
-        }
-        return $this->pairs;
-    }
-
-    private function inOrderPair($pair1, $pair2) {
-        $inOrder = 1;
-        if(gettype($pair1) != gettype($pair2)) {
-            (is_array($pair1) ? $pair2 = json_decode($pair2) : $pair1 = json_decode($pair1));
+        if(is_int($line1) && is_int($line2)) {
+            return $line1 <=> $line2;
         }
 
-        if(count($pair1) < count($pair2)) { echo "array smaller<br>"; return $inOrder; }
-        for($i=0; $i<count($pair1); $i++) {
-            echo "compare ". $pair1[$i] ." vs ". $pair2[$i] ."<br>";
-            if($pair1[$i] > $pair2[$i]) {
-                $inOrder = 0;
+        if(is_int($line1) && is_array($line2)) { $line1 = [$line1]; }
+        if(is_int($line2) && is_array($line1)) { $line2 = [$line2]; }
+
+        if(is_array($line1) && is_array($line2)) {
+            while(count($line1) && count($line2)) {
+
+                $l = array_shift($line1);
+                $r = array_shift($line2);
+
+                if($result = $this->compareLines($l, $r)) return $result;
             }
         }
-        return $inOrder;
+        return count($line1) - count($line2);
     }
-    */
+
+    public function addPackage(array $package) {
+        array_push($this->inputData, $package);
+    }
 }
