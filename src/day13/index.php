1c134d6a33551a6409b48d907a7c8c8aa6df389f
rmfloris <ralffloris@gmail.com>
2022-12-13 13:51:31 +0000

    update logica for puzzle 1, day 13

--- src/day13/index.php
+++ src/day13/index.php
@@ -3,14 +3,21 @@ require_once '../autoload.php';
 error_reporting(E_ALL & ~E_WARNING);
 use day13\utils\Day13;
 
-$filename = "input/input_test.txt";
-// $filename = "input/input.txt";
+// $filename = "input/input_test.txt";
+$filename = "input/input.txt";
 echo "<pre>";
 
-$path = new Day13($filename);
+$distress = new Day13($filename);
+$distress->startComparing();
 echo "<p>";
-echo "are these the same?";
-print_r($path->startComparing());
-    
+echo "are these the same?<p>";
+echo "Part 1, sum of pairs: ". $distress->getSumOfIndices() ." (first guess 5841, too high)";
+/**
+ * value of 5841 is too high
+ * number of pairs is correct
+ * all pairs have either a 0 or 1 value
+ */
+
+echo "<p>Pairs:" . print_r($distress->getPairs(), true) ."<br>";
 // echo "<pre>";
 // var_dump($path);
--- src/day13/utils/Day13.php
+++ src/day13/utils/Day13.php
@@ -15,9 +15,8 @@ class Day13 {
     private function parseData($filename) {
         $lines = (new LoadInput)->loadFileToLines($filename);
         foreach($lines as $line) {
-            if(json_decode($line) == null) {
-                continue;
-            } else {
+            if(strlen($line > 0)) {
+                // echo "lineimport: ". print_r(json_decode($line),true) ."<br>";
                 $this->inputData[] = json_decode($line);
             }
         }
@@ -26,10 +25,22 @@ class Day13 {
     public function startComparing() {
         $pair = 1;
         for($i=0; $i<count($this->inputData);$i++) {
+            // echo "<p>== Pair ". $pair ." ==<br>";
             $this->pairs[$pair] = $this->compareLines($this->inputData[$i], $this->inputData[$i+1]);
+
             $i++;
             $pair++;
         }
+        return $this->pairs;
+    }
+
+    public function getSumOfIndices() {
+        return array_sum(array_keys(array_filter($this->pairs, function($v, $k) {
+            return $v > 0;
+        }, ARRAY_FILTER_USE_BOTH)));
+    }
+
+    public function getPairs() {
         return $this->pairs;
     }
 
@@ -38,23 +49,37 @@ class Day13 {
 
         // echo "line 1: ". print_r($line1, true) ."<br>";
         // echo "line 2: ". print_r($line2, true) ."<br>";
+        // echo "- Compare [". implode(",", $line1) ."] vs [". implode(",", $line2) ."]<br>";
         for($i=0; $i<count($line1);$i++) {
+            // mixed types
+            //
+            // echo "Line: ". print_r($line1, true) ."<br>";
             if(getType($line1[$i]) != gettype($line2[$i])) {
+                // echo "type different<br>";
                 $status = $this->convertToArrayAndCompare($line1[$i], $line2[$i]);
                 if($status != -1) return $status;
             }
+            // compare pairs
+            //
             $status = $this->comparePair($line1[$i], $line2[$i]);
             if($status != -1) return $status;
         }
+        if($this->compareArrayLength($i, $line1, $line2) != -1) { return $this->compareArrayLength($i, $line1, $line2); }
         return $status;
     }
 
     private function convertToArrayAndCompare($line1, $line2) {
+        if(is_null($line2)) { return 0; }
         (is_array($line1) ? $line2 = [$line2] : $line1 = [$line1]);
         return $this->compareLines($line1, $line2);
     }
 
+    private function compareArrayLength($position, $array1, $array2) {
+        return (count(array_slice($array1, $position)) == 0 && count(array_slice($array2, $position)) > 0 ? 1: 0);
+    }
+
     private function comparePair($pair1, $pair2) {
+        // echo "  - Compare ". $pair1 ." vs ". $pair2 ."<br>";
         if($pair1 == $pair2) return -1;
         if($pair1 < $pair2) return 1;
         return 0;
