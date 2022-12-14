8228d775554f664e3afd56783b9897b78c38581e
rmfloris <ralffloris@gmail.com>
2022-12-14 21:00:04 +0000

    solved puzzle 1, day 13

--- src/day10/index.php
+++ src/day10/index.php
@@ -14,7 +14,7 @@ echo "1.Total signal strenght: " . $computer->getSignalStrength() ." (14780)<br>
 <html>
     <head>
         <title>Day 10 - AOC</title>
-    <link type="text/css" href="style.css" rel="stylesheet" />
+    <link type="text/css" href="../common/style.css" rel="stylesheet" />
     </head>
     <body>
         2. Answer: (ELPLZGZL)
