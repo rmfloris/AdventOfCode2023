2b230cd2ecac531b5094e148c47a6c61fa87d3cb
rmfloris <ralffloris@gmail.com>
2023-12-10 12:21:21 +0000

    addeding day 10

---
+++ src/day10/Day10.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace day10;
+
+use common\Day;
+
+class Day10 extends Day {
+
+    public function part1(): int {
+        return 1;
+    }
+
+    public function part2(): int {
+        return 1;
+    }
+    
+}
---
+++ tests/Day10/day10Test.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use day10\Day10;
+
+final class day10Test extends TestCase
+{
+    private mixed $day;
+    private mixed $sampleDay;
+
+    protected function setUp(): void
+    {
+        $this->day = new Day10();
+        $this->sampleDay = new Day10(true);
+    }
+    
+    public function testPart1Sample(): void
+    {
+        $this->markTestIncomplete("To be done");
+        // $this->assertSame(4361, $this->sampleDay->part1());
+    }
+
+    public function testPart1(): void
+    {
+        $this->markTestIncomplete("To be done");
+        // $this->assertSame(553079, $this->day->part1());
+    }
+
+    public function testPart2Sample(): void
+    {
+        $this->markTestIncomplete("To be done");
+        // $this->assertSame(467835, $this->sampleDay->part2());
+    }
+
+    public function testPart2(): void
+    {
+        $this->markTestIncomplete("To be done");
+        // $this->assertSame(84363105, $this->day->part2());
+    }
+}
